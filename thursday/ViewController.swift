f1a2f3fd5eda28aab03baf631f45bc15e46d0249
Chris <withscottc921@gmail.com>
2021-01-14 12:07:17 -0700

    Initial Commit

---
+++ thursday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thursday
+//
+//  Created by Chris Withers on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
